49f21eda58fa2d00993c4122d809150c8b4bdd8b
denys <example@gmail.com>
2020-04-06 17:20:58 +0300

    homework#8 commit

--- homework#8(landing page)/js/script.js
+++ homework#8(landing page)/js/script.js
@@ -6,6 +6,20 @@ window.addEventListener('DOMContentLoaded', function() {
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
 
+        let hideTabContent = (item, elem) => {
+            for (let i = item; i < elem.length; i++) {
+                elem[i].classList.remove('show');
+                elem[i].classList.add('hide');
+            }
+        };
+    
+        let showTabContent = (index, elem) => {
+            if (elem[index].classList.contains('hide')) {
+                    elem[index].classList.remove('hide');
+                    elem[index].classList.add('show');
+            }
+        };
+
         hideTabContent(1, tabContent);
 
         info.addEventListener('click', function(event) {
